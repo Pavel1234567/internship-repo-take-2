20c96759ef805dc0b2ce446f5df5056893029663
Pavel1234567 <pavel_azhel@rambler.ru>
2019-02-12 14:48:25 +0300

    продолжаю разрабатывать CPB

--- app/src/main/java/com/andersen/internship/testproject/CustomProgressBar.java
+++ app/src/main/java/com/andersen/internship/testproject/CustomProgressBar.java
@@ -1,4 +1,50 @@
 package com.andersen.internship.testproject;
 
-public class CustomProgressBar {
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+public class CustomProgressBar extends ProgressBar {
+    public CustomProgressBar(Context context) {
+        super(context);
+    }
+
+    public CustomProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public synchronized void setProgress(int progress) {
+        super.setProgress(progress);
+
+        if (progress <= 10){
+            setColor(Color.YELLOW);
+
+        } else if(progress <= 30){
+            setColor(Color.RED);
+
+
+        }else if(progress <= 70){
+            setColor(Color.BLUE);
+
+
+        }else if(progress <= 100){
+            setColor(Color.GREEN);
+
+        }
+
+    }
+
+    private void setColor(int color){
+        Drawable progressDrawable = getProgressDrawable().mutate();
+        progressDrawable.setColorFilter(color, android.graphics.PorterDuff.Mode.SRC_IN);
+        setProgressDrawable(progressDrawable);
+
+    }
 }
