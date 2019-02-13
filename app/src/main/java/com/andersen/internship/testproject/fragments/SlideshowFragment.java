ee7ce6c7c6b7fda6dbc2106a2836a683cd166217
Pavel1234567 <pavel_azhel@rambler.ru>
2019-02-13 10:45:17 +0300

    добавил отписку

--- app/src/main/java/com/andersen/internship/testproject/fragments/SlideshowFragment.java
+++ app/src/main/java/com/andersen/internship/testproject/fragments/SlideshowFragment.java
@@ -3,7 +3,6 @@ package com.andersen.internship.testproject.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +16,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class SlideshowFragment extends AbstractFragment {
 
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public SlideshowFragment() {
         setIdTitle(R.string.slideshow);
@@ -48,7 +51,12 @@ public class SlideshowFragment extends AbstractFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Flowable.interval(50, TimeUnit.MILLISECONDS)
+        if (compositeDisposable.size() > 0){
+            compositeDisposable.dispose();
+        }
+        compositeDisposable = new CompositeDisposable();
+
+        Disposable disposable = Flowable.interval(50, TimeUnit.MILLISECONDS)
                 .take(100)
                 .map(aLong -> {
                     int i = aLong.intValue();
@@ -57,9 +65,13 @@ public class SlideshowFragment extends AbstractFragment {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(i -> progressBar.setProgress(i));
+
+        compositeDisposable.add(disposable);
     }
 
-
-
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        compositeDisposable.dispose();
+    }
 }
