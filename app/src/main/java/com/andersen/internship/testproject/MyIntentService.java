589b7ffef1d66d46839b00d80698cef7dc3d92cc
Pavel1234567 <pavel_azhel@rambler.ru>
2019-02-25 20:30:40 +0300

    можно останавливть сервис

--- app/src/main/java/com/andersen/internship/testproject/MyIntentService.java
+++ app/src/main/java/com/andersen/internship/testproject/MyIntentService.java
@@ -3,14 +3,10 @@ package com.andersen.internship.testproject;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.os.ResultReceiver;
 import android.util.Log;
 
-import com.andersen.internship.testproject.activities.MainActivity;
 import com.andersen.internship.testproject.models.DummyServer;
-import com.andersen.internship.testproject.mvp.multithreading.Presenter;
 import com.andersen.internship.testproject.presenters.MultithreadingPresenter;
 
 import java.util.List;
@@ -27,6 +23,14 @@ public class MyIntentService extends IntentService {
 
     public static final int DATA = 1;
 
+    private ServiceBinding binder = new ServiceBinding();
+
+    public void setStopped(boolean stopped) {
+        isStopped = stopped;
+    }
+
+    private boolean isStopped = false;
+
     public MyIntentService() {
         super("myname");
     }
@@ -37,7 +41,7 @@ public class MyIntentService extends IntentService {
         int size = intent.getIntExtra(SIZE, 0);
         List<Double> list = DummyServer.getDummyServer().getDummyData(size);
         String result = MultithreadingPresenter.getPresenter().handleData(list);
-
+        if (isStopped) return;
         Intent intentGiveBack = new Intent(BROADCAST_ACTION);
         intentGiveBack.putExtra(RECEIVED_TYPE, DATA);
         intentGiveBack.putExtra(MESSAGE, result);
@@ -48,5 +52,15 @@ public class MyIntentService extends IntentService {
     public void onDestroy() {
         super.onDestroy();
         Log.d("myLogs", "onDestroy");
+    }
+
+    public IBinder onBind(Intent arg0) {
+        return binder;
+    }
+
+    public class ServiceBinding extends Binder {
+        public MyIntentService getService() {
+            return MyIntentService.this;
+        }
     }
 }
